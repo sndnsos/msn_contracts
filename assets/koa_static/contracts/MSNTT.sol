7fe9301851544547245b434f9c5866646179d2f8
leo <leo@leodeMacBook-Pro.local>
2021-08-07 10:08:20 +0800

    update

--- assets/koa_static/contracts/MSNTT.sol
+++ assets/koa_static/contracts/MSNTT.sol
@@ -3,24 +3,48 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MSNTT is ERC20 {
-    
-    bool exchange_open; //test-token lock , can only be exchanged after main-net
+
+    //test-token exchange lock, no one can exchange at present 
+    bool exchange_open;  
     address contractOwner;
 
+    mapping(address=>bool) maintainers;
+
     modifier onlyContractOwner() {
-        require(msg.sender == contractOwner, 'This function could only be used by contractOwner');
+        require(msg.sender == contractOwner, 'only contractOwner');
         _;
     }
 
     modifier onlyExchangeOpen() {
-        require(exchange_open == true, 'This function could only be used after exchange open ');
+        require(exchange_open == true, 'exchange closed');
+        _;
+    }
+
+    modifier MaintainerORExchangeOpen() {
+        require(exchange_open == true || maintainers[msg.sender]==true, 
+        'exchange closed && not contractOwner');
         _;
     }
 
     constructor() ERC20("MesonNetworkTestToken", "MSNTT") {
        contractOwner = msg.sender;
+       maintainers[msg.sender]=true;
        exchange_open=false;
         _mint(msg.sender, 500_000_000 * (10 ** uint256(decimals())));     
+    }
+
+    function add_maintainer(address maintainer) public onlyContractOwner {
+        maintainers[maintainer]=true;
+    }
+
+    function remove_maintainer(address maintainer) public onlyContractOwner {
+        if(maintainer!=contractOwner){
+               delete maintainers[maintainer];
+        }
+    }
+
+    function is_maintainer(address maintainer) public view returns (bool){
+        return maintainers[maintainer];
     }
 
     //make sure totalsupply keep synchronized to meson.network
@@ -41,27 +65,35 @@ contract MSNTT is ERC20 {
           return exchange_open;
     }
 
-    function transfer(address recipient, uint256 amount) public override returns (bool) {
-        if(msg.sender!=contractOwner&&!exchange_open){
-            return false; 
-        } 
+    function transfer(address recipient, uint256 amount) public override  MaintainerORExchangeOpen returns (bool) {
         return super.transfer(recipient, amount);
     }
 
-    function transferFrom( address sender, address recipient, uint256 amount ) public override onlyExchangeOpen returns (bool){
+    function transferFrom( address sender, address recipient, uint256 amount ) public override MaintainerORExchangeOpen returns (bool){
         return super.transferFrom( sender, recipient, amount);
     }
 
-    function  approve(address spender, uint256 amount) public override onlyExchangeOpen returns (bool) {
+    function  approve(address spender, uint256 amount) public override MaintainerORExchangeOpen returns (bool) {
          return super.approve( spender, amount);
     }
 
-    function decreaseAllowance(address spender, uint256 subtractedValue) public override onlyExchangeOpen returns (bool){
+    function decreaseAllowance(address spender, uint256 subtractedValue) public override MaintainerORExchangeOpen returns (bool){
         return super.decreaseAllowance( spender, subtractedValue);
     }
 
-    function increaseAllowance(address spender, uint256 addedValue) public override onlyExchangeOpen returns (bool){
+    function increaseAllowance(address spender, uint256 addedValue) public override MaintainerORExchangeOpen returns (bool){
         return super.increaseAllowance( spender, addedValue);
+    }
+ 
+    ///when user deposit here ,web test token will be added in meson.network
+    event DepositEVENT(address indexed _from, uint256 amount);
+ 
+    function Deposit(uint256 amount) public returns (bool) {
+        bool result= super.transfer(contractOwner, amount);
+        if(result){
+             emit DepositEVENT(msg.sender, amount);
+        }
+        return result;
     }
 
     
